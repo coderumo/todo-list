a7b068c7bec753576962da10284919fc503da6b1
rumeysa <sagtwix5@gmail.com>
2022-08-17 23:09:39 +0300

    couldn't solve the error

---
+++ todo.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelector(".card-body")[0];
+const secondCardBody = document.querySelector(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addTodo);
+}
+
+function addTodo(e) {
+    const newTodo = todoInput.value; //hata ne!?!?
+    console.log(newTodo)
+    e.preventDefault();
+}
